043ce83725ef226af31f4914681322d5e7e8bac3
earth037 <prithviprasad91@gmail.com>
2026-05-20 23:40:42 +0530

    Day 2 Java Variables

---
+++ Day-2/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args){
+        //integers
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 100000;
+
+        //Real Numbers
+        float f = 10.45f;
+        double d = 345.5344;
+
+        //Characters
+        char c = 'a';
+
+        //Boolean
+        boolean bool = true;
+
+        System.out.println("Integer values -->" + b + " , " + s + " , " + i + " , " + l);
+        System.out.println("Floating values -->" + f + " , " + d);
+        System.out.println("Character value -->" + c);
+        System.out.println("Boolean value -->" + bool);
+    }
+}
